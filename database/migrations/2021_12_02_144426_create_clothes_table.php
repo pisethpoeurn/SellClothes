c5dab8110c0196d170da121713feddfcfbff5c05
pisethpoeurn <pisethpoeurn622@gmail.com>
2021-12-02 22:07:13 +0700

    add config project

---
+++ database/migrations/2021_12_02_144426_create_clothes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClothesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clothes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('picture')->nullable();
+            $table->double('price')->nullable();
+            $table->string('decription')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clothes');
+    }
+}
